c949b74ef609ea560263b0921ea3005dd4512eb0
Arno Rehn <arno@arnorehn.de>
2015-10-14 16:52:48 +0200

    Finish the Scan class, fix some stuff and add docs

--- common.py
+++ common.py
@@ -70,6 +70,23 @@ class Manipulator(ComponentBase):
     def value(self):
         return None
 
+    async def beginScan(self, minimumValue):
+        """ Moves the manipulator to the starting value of a following continuous scan.
+
+        Typically, this will only need to be re-implemented by manipulators emitting trigger signals: If
+        `minimumValue` coincides with a trigger position, the initial trigger pulse might not be emitted at all
+        if movement started at exactly `minimumValue`.
+        Hence, this method should move the manipulator slightly in front of `minimumValue` so that the first
+        trigger actually corresponds to `minimumValue`.
+
+        The default implementation simply awaits ``moveTo(minimumValue)``.
+
+        Parameters
+        ----------
+        minimumValue (numeric) : The initial position.
+        """
+        await self.moveTo(minimumValue)
+
     async def moveTo(self, val):
         pass
 
@@ -124,3 +141,11 @@ class DataSet:
     @axes.setter
     def axes(self, val):
         self._axes = val
+
+    def __repr__(self):
+        return 'DataSet(%s, %s)' % (repr(self.data), repr(self.axes))
+
+    def __str__(self):
+        return 'DataSet with:\n    %s\n  and axes:\n    %s' % \
+        (repr(self.data).replace('\n', '\n    '), \
+         repr(self.axes).replace('\n', '\n    '))
--- scan.py
+++ scan.py
@@ -17,19 +17,37 @@ class Scan(DataSource):
         self.dim = dim
         self.minimumValue = minimumValue
         self.maximumValue = maximumValue
-        self.step = 0
+        self.step = step
         self.continuousScan = False
         self.retractAtEnd = False
 
     async def _doContinuousScan(self, axis):
-        await self.manipulator.moveTo(self.minimumValue - self.step)
+        await self.manipulator.beginScan(self.minimumValue)
         self.dataSource.start()
         await self.manipulator.moveTo(self.maximumValue)
         self.dataSource.stop()
 
         dataSet = await self.dataSource.readDataSet()
+        if dataSet.data.ndim != len(dataSet.axes):
+            raise Exception("Axes/data mismatch. Data dimension: %d, number of axes: %d" %
+                            (dataSet.data.ndim, len(dataSet.axes)))
+
         dataSet.axes = dataSet.axes.copy()
         dataSet.axes[0] = axis
+
+        expectedLength = len(dataSet.axes[0])
+
+        # Oops, we have more data points than axis points...
+        if dataSet.data.shape[0] != expectedLength:
+            # If we're only off by one, then the maximumValue has probably triggered another acquisition.
+            # Simply drop the last one.
+            if dataSet.data.shape[0] == expectedLength + 1:
+                dataSet.data = dataSet.data[:-1]
+            else:
+                raise Exception("Length of recorded data set does not match expectation. "
+                                "Actual length: %d, expected length: %d" %
+                                (dataSet.data.shape[0], expectedLength))
+
         return dataSet
 
     async def _doSteppedScan(self, axis):
@@ -55,9 +73,9 @@ class Scan(DataSource):
         dataSet = None
 
         if self.continuousScan:
-            dataSet = await self._doContinuousScan(self, axis)
+            dataSet = await self._doContinuousScan(axis)
         else:
-            dataSet = await self._doSteppedScan(self, axis)
+            dataSet = await self._doSteppedScan(axis)
 
         if self.retractAtEnd:
             await self.manipulator.moveTo(self.minimumValue)
