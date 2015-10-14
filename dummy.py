3475c8775d8c00c68fdc6399ae9acbecfe319eab
Arno Rehn <arno@arnorehn.de>
2015-10-14 16:53:23 +0200

    add Dummy classes and a testing script

---
+++ dummy.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 14:56:08 2015
+
+@author: pumphaus
+"""
+
+from common import DataSource, Manipulator, DataSet
+import numpy as np
+
+class DummyManipulator(Manipulator):
+    pass
+
+class DummySimpleDataSource(DataSource):
+    def __init__(self, init = 0):
+        super().__init__()
+        self.init = init
+        self.stop()
+
+    def stop(self):
+        self.counter = self.init
+
+    async def readDataSet(self):
+        dataSet = DataSet(np.array(self.counter), [])
+        self.counter += 1;
+        return dataSet
+
+class DummyContinuousDataSource(DataSource):
+    def __init__(self, init, count):
+        super().__init__()
+        self.init = init
+        self.count = count
+
+    async def readDataSet(self):
+        dataSet = DataSet(np.arange(self.init, self.init + self.count), [ np.arange(0, self.count) ])
+        return dataSet
---
+++ test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 15:04:51 2015
+
+@author: pumphaus
+"""
+
+from scan import Scan
+from dummy import DummyManipulator, DummySimpleDataSource, DummyContinuousDataSource
+import numpy as np
+import asyncio
+
+async def testRun():
+    manip = DummyManipulator()
+    sourceA = DummyContinuousDataSource(init = 42, count = 10)
+    scan = Scan(manip, sourceA, minimumValue = 0, maximumValue = 10, step = 1)
+    scan.continuousScan = True
+    scanB = Scan(DummyManipulator(), scan, minimumValue = 0, maximumValue = 1.5, step = 1)
+
+    dataSet = await scanB.readDataSet()
+    print(dataSet)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(testRun())
