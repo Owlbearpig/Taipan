0fb18d5085194c4ebf39f0052156d587067877a9
Arno Rehn <arno@arnorehn.de>
2015-10-16 14:08:39 +0200

    Add PI Connection type

---
+++ stages/PI/Connection.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 16 09:14:38 2015
+
+@author: Arno Rehn
+"""
+
+from common import ComponentBase
+from asyncioext.asyncserial import Serial
+
+class Connection(ComponentBase):
+    def __init__(self, port = None, baudRate = 9600):
+        super().__init__()
+        self.port = port
+        self.baudRate = baudRate
+        self.serial = Serial()
+
+    def __del__(self):
+        self.close()
+
+    def open(self):
+        """ Opens the Connection, potentially closing an old Connection
+        """
+        self.close()
+        self.serial.port = self.port
+        self.serial.open()
+
+    def close(self):
+        """ Closes the Connection.
+        """
+        if self.serial.isOpen():
+            self.serial.close()
+
+    async def send(self, command):
+        """ Send a command over the Connection. If the command is a request,
+        returns the reply.
+
+        Parameters
+        ----------
+        command (convertible to bytearray) : The command to be sent.
+        """
+
+        # convert `command` to a bytearray
+        if isinstance(command, str):
+            command = bytearray(command, 'ascii')
+        else:
+            command = bytearray(command)
+
+        if command[-1] != b'\n': command += b'\n'
+
+        self.serial.write(command)
+
+        # no request -> no reply. just return.
+        if command[-2] != ord(b'?'):
+            return
+
+        # read reply. lines ending with ' \n' are part of a multiline reply.
+        replyLines = []
+        while len(replyLines) == 0 or replyLines[-1][-2:] == ' \n':
+            replyLines.append(await self.serial.async_readline())
+
+        return b''.join(replyLines)
---
+++ stages/PI/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 16 09:33:05 2015
+
+@author: Arno Rehn
+"""
+
+from .Connection import Connection
