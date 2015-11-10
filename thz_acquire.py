60a9aadb1847306bcd357a22e30096745cfa8582
Arno Rehn <arno@arnorehn.de>
2015-11-10 16:11:08 +0100

    update thz_acquire test-script

--- thz_acquire.py
+++ thz_acquire.py
@@ -12,6 +12,8 @@ from datasources import SR830
 import asyncio
 from stages import PI
 
+import asyncioext
+
 prologix_gpib.gpib_prologix_device = Serial('/tmp/sr830', baudrate=115200, timeout = 0.150)
 
 rm = visa.ResourceManager('@py')
@@ -24,19 +26,29 @@ async def setup():
     global sr830, conn, controller
 
     sr830 = SR830(rm.open_resource('GPIB0::10::INSTR'))
+    print(sr830.identification)
 
-    conn = PI.Connection()
+    conn = PI.Connection(baudRate = 9600)
     conn.port = '/tmp/pistage'
     conn.open()
 
     controller = PI.AxisAtController(conn)
     await controller.initialize()
-    print("PI Controller is: %s" % controller._identification)
-    if not controller.isReferenced:
-        await controller.reference()
+    print("PI Controller is: %s" % controller._identification, flush=True)
+    print("PI position: %s" % str(controller.value))
 
 async def run():
     await setup()
 
+    realVals = await controller.configureTrigger(0.1, 10, 20)
+    print(realVals)
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
+
+del controller
+del conn
+del sr830
+del prologix_gpib.gpib_prologix_device
+
+loop.run_until_complete(asyncio.sleep(1))
