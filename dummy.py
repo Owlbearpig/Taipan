0161b1d59a707d22fa381a5ad894b7cccbcaadee
Arno Rehn <arno@arnorehn.de>
2015-11-14 12:15:05 +0100

    Update dummy classes

--- dummy.py
+++ dummy.py
@@ -6,12 +6,34 @@ Created on Wed Oct 14 14:56:08 2015
 """
 
 from common import DataSource, Manipulator, DataSet
+import asyncio
 import numpy as np
 
 class DummyManipulator(Manipulator):
+    def __init__(self):
+        super().__init__()
+        self._val = 0
+
+    @property
+    def value(self):
+        return self._val
+
     @property
     def status(self):
         return self.Status.TargetReached
+
+    async def moveTo(self, val, velocity = None):
+        self.velocity = velocity
+        values = np.linspace(self._val, val, 20)
+        for target in values:
+            await asyncio.sleep(0.1)
+            self._val = target
+
+    async def configureTrigger(self, step, start = None, stop = None):
+        self.step = step
+        self.start = start
+        self.stop = stop
+        return step, start, stop
 
 class DummySimpleDataSource(DataSource):
     def __init__(self, init = 0):
@@ -28,12 +50,15 @@ class DummySimpleDataSource(DataSource):
         return dataSet
 
 class DummyContinuousDataSource(DataSource):
-    def __init__(self, init, count):
+    def __init__(self, init, count, manip):
         super().__init__()
         self.init = init
         self.count = count
+        self.manip = manip
 
     async def readDataSet(self):
-        dataSet = DataSet(np.arange(self.init, self.init + self.count),
-                          [ np.arange(0, self.count) ])
+        taxis = np.arange(self.manip.start, self.manip.stop, self.manip.step)
+        omega = 2 * np.pi * 5
+        data = np.sin(omega * taxis)
+        dataSet = DataSet(data, [ taxis ])
         return dataSet
