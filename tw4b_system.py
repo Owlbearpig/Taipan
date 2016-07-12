6eb897d8f86ea41103888d2ec4a074f9f052bca8
Arno Rehn <arno@arnorehn.de>
2016-07-12 11:30:10 +0200

    make the TW4B System example useful by adding data saving support

--- tw4b_system.py
+++ tw4b_system.py
@@ -6,9 +6,20 @@ Created on Thu Jul  7 09:17:32 2016
 """
 
 from datasources import TW4B
+from traitlets import Instance
+from common import action
+from common.save import DataSaver
 
 
 class AppRoot(TW4B):
 
+    dataSaver = Instance(DataSaver)
+
     def __init__(self):
-        super().__init__('192.168.134.16')
+        super().__init__('169.254.4.42', objectName="TeraFlash")
+        self.dataSaver = DataSaver(objectName="Data Saver")
+
+    @action("Take measurement")
+    async def takeMeasurement(self):
+        data = await self.readDataSet()
+        self.dataSaver.process(data)
