b9437b1e790c0344dcbe122a1aa2c8e0275d09c5
Arno Rehn <arno@arnorehn.de>
2016-07-31 12:32:35 +0200

    Add Copyright notices.

--- util/numpyjsonencoder.py
+++ util/numpyjsonencoder.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan  6 14:00:34 2016
+This file is part of Taipan.
 
-Based on http://stackoverflow.com/questions/3488934/simplejson-and-numpy-array/24375113#24375113
+Copyright (C) 2015 - 2016 Arno Rehn <arno@arnorehn.de>
 
-@author: tlausch
-@author: Def_Os
+Taipan is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Taipan is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Taipan.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import base64
