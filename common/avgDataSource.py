e4bab26fd86eda62beec56633ae3307d3bd7e406
David Jahn <David.Jahn@staff.uni-marburg.de>
2016-09-23 13:26:11 +0200

    Added dataset length check to averager

--- common/avgDataSource.py
+++ common/avgDataSource.py
@@ -4,7 +4,7 @@ Created on Thu Sep  8 12:21:50 2016
 
 @author: dave
 """
-from common import DataSource
+from common import DataSource, action
 from traitlets import Integer, Instance
 import logging
 
@@ -14,6 +14,10 @@ class AverageDataSource(DataSource):
 
     currentAverages = Integer(0,read_only = True).tag(
             name='current Averages')
+    
+    dataLen = Integer(0,read_only = True).tag(
+            name="Expected Data Length")
+
     singleSource = Instance(DataSource, allow_none=True)
 
     def __init__(self, dataSource, objectName=None, loop=None):
@@ -27,15 +31,31 @@ class AverageDataSource(DataSource):
         self.set_trait('currentAverages',1)
         self.singleSource.stop()
 
+    @action("expected length")
+    async def readDataLength(self):
+        cd = await self.singleSource.readDataSet()
+        self.set_trait('dataLen',len(cd.data))
+
     async def readDataSet(self):
+        if self.dataLen == 0:
+            logging.info("Please set the expected data length!, try to guess it")
+            await self.readDataLength()
+
         if self.numberofAverages <1:
             logging.info("Averaging: Please insert a positive number, averages set to 1")
             self.numberofAverages = 1
-
         avDataSet = await self.singleSource.readDataSet()
+        while len(avDataSet.data) != self.dataLen:
+            logging.info("Failed to read data with correct length, retry!")
+            avDataSet = await self.singleSource.readDataSet()
+          
         self.set_trait('currentAverages',1)
         while self.currentAverages<self.numberofAverages:
             singleSet = await self.singleSource.readDataSet()
+            while len(singleSet.data) != self.dataLen:
+                logging.info("Failed to read data with correct length, retry!")
+                singleSet = await self.singleSource.readDataSet()
+
             avDataSet.data += singleSet.data
             self.set_trait('currentAverages',self.currentAverages+1)
         avDataSet.data /= self.numberofAverages
