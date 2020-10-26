1a632545d160df33753ec6548510f6addfe86ef9
Owlbearpig <alexj07@gmail.com>
2020-10-26 20:13:26 +0100

    Hopefully this version actually works now? Should be all files from lab2 pc, not sure if I got everything ... Should probably test it =)

---
+++ thz_context.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 29 11:15:57 2016
+
+@author: Arno Rehn
+"""
+
+from pint import Context
+from common import ureg
+
+# Create and enable a THz-TDS context where we can convert times to lengths
+# and vice-versa
+thz_context = Context('terahertz')
+
+thz_context.add_transformation('[time]', '[length]',
+                               lambda ureg, x: ureg.speed_of_light * x / 2)
+thz_context.add_transformation('[length]', '[time]',
+                               lambda ureg, x: 2 * x / ureg.speed_of_light)
+
+thz_context.add_transformation('', '[length]/[time]',
+                               lambda ureg, x: ureg.speed_of_light * x / 2)
+thz_context.add_transformation('[length]/[time]', '',
+                               lambda ureg, x: 2 * x / ureg.speed_of_light)
+
+ureg.add_context(thz_context)
+ureg.enable_contexts('terahertz')
