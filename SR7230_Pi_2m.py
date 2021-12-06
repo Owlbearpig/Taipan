a0a5d6322d5cc46d8f37a46a6755440845148722
Kochbuch <jaeckel7@staff.uni-marburg.de>
2021-12-06 18:13:23 +0100

    - Replaced pi stages with dummy...

--- SR7230_Pi_2m.py
+++ SR7230_Pi_2m.py
@@ -15,6 +15,7 @@ import thz_context
 import os
 import traitlets
 from pathlib import Path
+from dummy import DummyManipulator
 
 if os.name == 'posix':
     rm = visa.ResourceManager('@py')
@@ -24,9 +25,9 @@ elif os.name == 'nt':
 SR7230_USB_Port = 'USB0::0x0A2D::0x0027::14043751::RAW'
 SR7230_LAN_Port = "TCPIP::169.254.150.230::50000::SOCKET"
 
-PI_comport = 'COM4'
-Manip1_comport = 'COM3'  # small stage ?
-Manip2_comport = 'COM5'  # big stage ?
+PI_comport = 'COM13'
+Manip1_comport = 'COM3'
+Manip2_comport = 'COM5'
 
 
 class AppRoot(TabularMeasurements2M):
@@ -47,10 +48,10 @@ class AppRoot(TabularMeasurements2M):
         self.dataSaver = DataSaver(objectName="Data Saver")
 
         self.pi_conn = PI.Connection(PI_comport)
-        self.mani_conn1 = PI.Connection(Manip1_comport)
-        self.mani_conn2 = PI.Connection(Manip2_comport)
+        #self.mani_conn1 = PI.Connection(Manip1_comport)
+        #self.mani_conn2 = PI.Connection(Manip2_comport)
 
-        pi_stage = PI.AxisAtController(self.pi_conn, ignore216=True)  # ignore216 implemented by JanO Warning!
+        pi_stage = PI.AxisAtController(self.pi_conn)
         pi_stage.objectName = "PI C-863 DLine"
         pi_stage.setPreferredUnits(ureg.ps, ureg.ps / ureg.s)
 
@@ -70,25 +71,25 @@ class AppRoot(TabularMeasurements2M):
 
         self.dataSource = self.TimeDomainScan
 
-        manipulator1 = PI.AxisAtController(self.mani_conn1)
+        manipulator1 = DummyManipulator()#PI.AxisAtController(self.mani_conn1)
         manipulator1.setPreferredUnits(ureg.mm, ureg.mm / ureg.s)
-        manipulator1.objectName = 'PI C-863 1'
+        manipulator1.objectName = 'DummyManipulator 1'
         self.manipulator1 = manipulator1
         self.positioningVelocityM1 = Q_(4, 'mm/s')
         self.scanVelocity = Q_(4, 'mm/s')
 
-        manipulator2 = PI.AxisAtController(self.mani_conn2)
+        manipulator2 = DummyManipulator()#PI.AxisAtController(self.mani_conn2)
         manipulator2.setPreferredUnits(ureg.mm, ureg.mm / ureg.s)
-        manipulator2.objectName = 'PI C-863 2'
+        manipulator2.objectName = 'DummyManipulator 2'
         self.manipulator2 = manipulator2
         self.positioningVelocityM1 = Q_(4, 'mm/s')
         self.scanVelocity = Q_(4, 'mm/s')
 
-        self.dataSaver.registerManipulator(self.manipulator1, 'Position smallStage')
-        self.dataSaver.registerManipulator(self.manipulator2, 'Position bigStage')
+        self.dataSaver.registerManipulator(self.manipulator1, 'Position1')
+        self.dataSaver.registerManipulator(self.manipulator2, 'Position2')
         self.dataSaver.registerObjectAttribute(self, 'currentMeasurementName', 'currentTableEntry')
-        self.dataSaver.fileNameTemplate = '{date}-{name}-{currentTableEntry}-{Position}-{Rotation}'
-        self.dataSaver.set_trait('path', Path('E:/Messdaten/test/'))  # added by Cornelius as standard savepath
+        self.dataSaver.fileNameTemplate = '{date}-{name}-{currentTableEntry}-{Position1}-{Position2}'
+        self.dataSaver.set_trait('path', Path(r'D:\MeasurementTest'))
         self.TimeDomainScan.addDataSetReadyCallback(self.dataSaver.process)
         self.TimeDomainScan.addDataSetReadyCallback(self.setCurrentData)
         self._backAndForth = True
@@ -99,8 +100,8 @@ class AppRoot(TabularMeasurements2M):
     async def __aenter__(self):
         # await super().__aenter__()
         await self.pi_conn.__aenter__()
-        await self.mani_conn1.__aenter__()
-        await self.mani_conn2.__aenter__()
+        #await self.mani_conn1.__aenter__()
+        #await self.mani_conn2.__aenter__()
         await self.manipulator1.__aenter__()
         await self.manipulator2.__aenter__()
         await self.TimeDomainScan.manipulator.__aenter__()  # pi
@@ -128,8 +129,8 @@ class AppRoot(TabularMeasurements2M):
 
     async def __aexit__(self, *args):
         await self.pi_conn.__aexit__(*args)
-        await self.mani_conn1.__aexit__(*args)
-        await self.mani_conn2.__aexit__(*args)
+        #await self.mani_conn1.__aexit__(*args)
+        #await self.mani_conn2.__aexit__(*args)
         await self.manipulator1.__aexit__(*args)
         await self.manipulator2.__aexit__(*args)
         await self.TimeDomainScan.manipulator.__aexit__(*args)  # pi
